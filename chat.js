c5e2a1225e00bcff73f4748144ec197790105bdf
Brandon Montgomery <bmonty@gmail.com>
2012-07-19 00:09:56 -0400

    docs for chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,16 @@
 var WebSocketServer = require('websocket').server;
 
+// # ChatUser
+// A user and a connection are the same thing in this system.
+// Represents a user who is connected to the chat room.
 var ChatUser = (function() {
+
+	// ## Constructor
+	//
+	// **svr**: The ChatServer
+	//
+	// **conn**: The WebSocket connection to which this ChatUser is
+	// associated.
 	function ChatUser(svr, conn) {
 		var self = this;
 		this.server = svr;
@@ -14,6 +24,11 @@ var ChatUser = (function() {
 		this.connection.on('message', messageReceived);
 	}
 
+	// ## sendMessage
+	//
+	// **message**: the message to send to the user
+	//
+	// Sends a message to the user.
 	ChatUser.prototype.sendMessage = function(message) {
 		console.log('sending message: ' + message);
 		this.connection.sendUTF(message);
@@ -22,8 +37,15 @@ var ChatUser = (function() {
 	return ChatUser;
 })();
 
+// # ChatServer
+//
+// The central object which manages users and ferries messages
+// to and from users connected to the server.
 exports.ChatServer = (function() {
-	// constructor
+	// ## Constructor
+	//
+	// **httpServer**: The web server to which to connect the Web Socket
+	// server to allow connections to the chat server.
 	function ChatServer(httpServer) {
 		var self = this;
 		this.users = [];
@@ -31,27 +53,39 @@ exports.ChatServer = (function() {
 			throw 'httpServer is required';
 		}
 
-		// web socket server
+		// Instantiate the Web Socket server
 		var socketServer = new WebSocketServer({
 			httpServer: httpServer
 		});
 
+		// Handles when a client connects to the Web Socket server
 		var connectionRequest = function(request) {
+			// Accept the Web Socket connection
 			var connection = request.accept(null, request.origin);
+
+			// Instantiate a ChatUser, and pass a reference to this
+			// server and the connection to which this user corresponds
 			var chatUser = new ChatUser(self, connection);
 			self.users.push(chatUser);
 		};
 
+		// Listen for Web Socket ocnnection requests
 		socketServer.on('request', connectionRequest);
 	}
 
+	// ## publicMessage
+	//
+	// **sender**: The ChatUser from which the message originated
+	//
+	// **message**: The message to send to the chat room
+	//
+	// Sends a message to all users connected to the Chat Server
+	// except for the user from which the message was sent.
 	ChatServer.prototype.publishMessage = function(sender, message) {
 		this.users.forEach(function(user) {
 			if (user != sender) {
 				user.sendMessage(message);
 			}
 		});
-	};
-
-	return ChatServer;
+	}
 })();
--- server.js
+++ server.js
@@ -2,30 +2,37 @@ var http = require('http');
 var fs = require('fs');
 var ChatServer = require('./chat').ChatServer;
 
-// basic web server functionality.
+// Start up a web server
 var server = http.createServer(function(request, response) {
-	// the read file callback (see below)
-	// gets the data from the file read operation and puts it into the
-	// response, then ends the response to send it to the client
+	
+	// When the index.html file is read, this callback is fired.
+	// If we can't find the file, we return a 404. If we do find
+	// the file, then we return a 200 with the contents of the
+	// file contained in the response.
 	var readFileCallback = function(err, data) {
 		if (err) {
+			// There was a problem reading the file. Return a 404.
 			response.writeHead(404);
 
+			// Log to the console if there's a problem to help
+			// troublehsooting.
 			console.error('error reading file: ');
 			console.error(err);
 		}
 
 		response.writeHead(200);
+
+		// Write the file's contents to the response.
 		response.write(data);
 		response.end();
 	};
 
-	// this reads the index.html file from disk and calls the callback
+	// Read the index.html from disk in this directory.
 	fs.readFile('index.html', readFileCallback);
 });
 
-// listen on a port
+// Listen on port 1337.
 server.listen(1337);
 
-// initialize the chat server
+// Initialize our chat server.
 var chatServer = new ChatServer(server);
